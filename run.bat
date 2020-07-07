01e61635cfbcff006a9a8ccfbea286b7b2b263c0
blaspheme <342843739@qq.com>
2020-07-07 18:01:52 +0800

    修改ubuntu 16的基础镜像为64位

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-docker run --rm -it --name xv6 -v /root/6.828/:/6.828 erudite/mit-ubuntu:1.0 /bin/bash
+docker stop xv6
+docker run --rm -it --name xv6 -v /root/6.828/lab/:/6.828 erudite/mit-ubuntu:1.0 /bin/bash
